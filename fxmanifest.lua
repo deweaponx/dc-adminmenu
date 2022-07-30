9486d7b92868feba0a585d0566de5e7761b1557c
ChatDisabled <44729807+ChatDisabled@users.noreply.github.com>
2022-07-30 14:36:08 +0200

    bumb version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'Disabled Coding'
 description 'Admin menu specifically made for the QBCore framework'
-version '2.1.0'
+version '2.1.1'
 repository 'https://github.com/Disabled-Coding/dc-adminmenu'
 
 shared_scripts {
